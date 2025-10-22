11d5a864eb5449a365c2e8caba6c10046f253e03
Solomon <olurebisolomon@gmail.com>
2025-10-22 11:33:03 -0700

    added delay to the function added a loader for the delay

--- script.js
+++ script.js
@@ -17,27 +17,37 @@ setInterval(updateClock, 1000);
 // Date functionality
 const date = new Date();
 const todaysDate = date.toLocaleDateString();
-document.getElementById("date").innerHTML =`Today's date is ${todaysDate}`;
+document.getElementById("date").innerHTML = `Today's date is ${todaysDate}`;
 
 // Quote genrator functionality
 // fetch("https://api.quotable.io/random")
 // .then(response => response.json())
 // .then(data =>{
 //     console.log(data.content);
-    
-// });
 
+// });
+document.querySelector(".loader").style.display = "none";
 
 async function getQuote() {
-    try{
-        const response = await fetch("https://api.quotable.io/random");
-        const data = await response.json()
-        console.log(data);
-        document.querySelector(".quote-text").innerHTML = data.content;
-        document.querySelector(".quote-author").innerHTML = data.author;
-    }catch (err){
-        console.error(err)
-    }
+  document.querySelector(".loader").style.display = "block";
+  document.querySelector(".quote-text").innerHTML = "";
+  document.querySelector(".quote-author").innerHTML = "";
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // used to intentionally delay the fetch
+
+    const response = await fetch(
+      "https://quoteslate.vercel.app/api/quotes/random"
+    );
+    const data = await response.json();
+    console.log(data);
+
+    document.querySelector(".quote-text").innerHTML = data.quote;
+    document.querySelector(".quote-author").innerHTML = data.author;
+    document.querySelector(".loader").style.display = "none";
+    // document.querySelector(".quote-text").innerHTML = '';
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-getQuote()
+getQuote();
