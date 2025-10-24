f5cab213a874447740e46c76c050fc3b107dd079
Solomon <olurebisolomon@gmail.com>
2025-10-24 06:20:12 -0700

    added functionality to the favourites section , enabled saving ability

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ const user_name = localStorage.getItem("userName");
 // console.log(user_name);
 let firstLetter = user_name.slice(0, 1).toUpperCase();
 let editedName = firstLetter + user_name.slice(1);
+let time = new Date().toLocaleTimeString();
+console.log(time);
+if (time.includes("AM")) {
+  console.log(true);
+} else if (time.includes("PM")) {
+  console.log(false);
+}
 document.querySelector(".greeting").innerHTML = `Good morning, ${editedName}`;
+
+// Dark mode toggle
+document.querySelector(".theme-toggle-btn").addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+});
 
 // CLOCK functionality
 const updateClock = () => {
@@ -19,13 +31,6 @@ const date = new Date();
 const todaysDate = date.toLocaleDateString();
 document.getElementById("date").innerHTML = `Today's date is ${todaysDate}`;
 
-// Quote genrator functionality
-// fetch("https://api.quotable.io/random")
-// .then(response => response.json())
-// .then(data =>{
-//     console.log(data.content);
-
-// });
 document.querySelector(".loader").style.display = "none";
 
 async function getQuote() {
@@ -33,7 +38,7 @@ async function getQuote() {
   document.querySelector(".quote-text").innerHTML = "";
   document.querySelector(".quote-author").innerHTML = "";
   try {
-    await new Promise((resolve) => setTimeout(resolve, 1000)); // used to intentionally delay the fetch
+    await new Promise((resolve) => setTimeout(resolve, 500)); // used to intentionally delay the fetch
 
     const response = await fetch(
       "https://quoteslate.vercel.app/api/quotes/random"
@@ -51,3 +56,83 @@ async function getQuote() {
 }
 
 getQuote();
+
+// Saving to favourites
+function getCurrentQuote() {
+  const id = new Date();
+  const quoteEl = document.querySelector(".quote-text");
+  const authorEl = document.querySelector(".quote-author");
+  const quote = quoteEl ? quoteEl.innerText.trim() : "";
+  const author = authorEl ? authorEl.innerText.trim() : "";
+  return { id, quote, author };
+}
+
+function saveToFavourites(quoteObj) {
+  let favourites = JSON.parse(localStorage.getItem("favourites")) || [];
+
+  const quoteExists = favourites.some(
+    (fav) => fav.quote === quoteObj.quote && fav.author === quoteObj.author
+  );
+
+  if (!quoteExists) {
+    favourites.push(quoteObj);
+    localStorage.setItem("favourites", JSON.stringify(favourites));
+    console.log(favourites);
+  } else {
+    alert("quote alraedy exists");
+  }
+  // console.log(favourites);
+}
+
+document.querySelector('#saveQuoteBtn').addEventListener('click' , () => {
+    const favouriteQuote = getCurrentQuote();
+
+    if(favouriteQuote.quote){
+      saveToFavourites(favouriteQuote)
+      alert('Quote has been saved')
+      // console.log(favourites)
+    }else{
+      alert('no quote was found to save')
+    }
+})
+
+function displayFavourites() {
+  const favourites = JSON.parse(localStorage.getItem("favourites") || []);
+  console.log(favourites);
+
+  const container = document.querySelector("#favourites-section");
+
+  if (favourites.lenght === 0) {
+    container.innerHTML = `<p>No favourites yet</p>`;
+    return;
+  }
+
+  favourites.forEach((fav) => {
+    container.innerHTML += `
+    <div class="quote-card">
+      <p class="quote">"${fav.quote}"</p>
+      <p class="author">— ${fav.author}</p>
+      <button class="remove-btn">Remove</button>
+    </div>`;
+  });
+}
+
+// displayFavourites();
+
+function removeQuote(id){ 
+  const favourites = JSON.parse(localStorage.getItem("favourites") || []);
+
+ const updatedFavourites = favourites.filter(fav => fav.id !== id);
+}
+
+
+removeQuote();
+
+// switch display
+// function
+
+document.querySelector(`#home`).addEventListener('click', () =>{
+  document.querySelector('#first').classList.toggle( "hidden") 
+  document.querySelector(`#favourites-section`).classList.toggle("hidden");
+  // console.log('clicked')
+})
